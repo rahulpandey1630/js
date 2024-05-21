7d8fa94ebb8c07f6e8d0661c3d931767aa62e820
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-05-21 13:09:23 +0000

    container config done

--- 01_basics/01variables.js
+++ 01_basics/01variables.js
@@ -1,17 +1,11 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+const accountId = 144553; // `const` is used for variables that shouldn't change
+let accountEmail = "hitesh@google.com"; // `let` is used for variables that can change
+var accountPassword = "12345"; // `var` is function-scoped and should be avoided in modern JavaScript
+accountCity = "Jaipur"; // Without `let`, `const`, or `var`, this creates a global variable (not recommended)
+let accountState; // `let` allows declaration without initialization
 
-// accountId = 2 // not allowed
-
-
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+// The following line would throw an error because `accountId` is a constant
+// accountId = 2; // not allowed
 
 /*
 Prefer not to use var
