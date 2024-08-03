df0fdcd92994ec35b9398900b614f087009ea73b
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-08-03 17:40:19 +0000

    container config done

--- 01_basics/01variables.js
+++ 01_basics/01variables.js
@@ -14,3 +14,19 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*var vs let in JavaScript
+Scope
+var: Function-scoped.
+let: Block-scoped.
+Hoisting
+var: Hoisted and initialized with undefined.
+let: Hoisted but not initialized (temporal dead zone).
+Re-declaration
+var: Allows re-declaration within the same scope.
+let: Does not allow re-declaration within the same scope.
+Usage
+var: Older code, backward compatibility.
+let: Modern code, preferred due to block-scoping and fewer bugs.
+Summary
+Use let for block-scoped variables.
+Use var for function-scoped variables or older code compatibility.*/
